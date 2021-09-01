7fa34e76b289332372cbc71a1755a14812effa2e
Angela Scott <sco308@pearcey-i3.cm.hpc>
2021-09-01 10:28:12 +1000

    update

--- run_rmarkdown.R
+++ run_rmarkdown.R
@@ -1,6 +1,7 @@
 
 library(rmarkdown)
 library(optparse)
+library(officedown)
 
 # use optparse to grab command line arguments for the report
 
@@ -19,9 +20,7 @@ option_list <- list(
   make_option(c("--tree"), type="character", default=NULL,
               help="Phylogenetic tree in image format", metavar="character"),
   make_option(c("--HA_gene"), type="character", default=NULL,
-              help="HA sequence in fasta format", metavar="character"),
-  make_option(c("--datasets"), type="character", default=NULL,
-              help="Sequence datasets used for comparison purposes", metavar="character")
+              help="HA sequence in fasta format", metavar="character")
 )
 
 
@@ -30,13 +29,15 @@ opt <- parse_args(opt_parser)
 
 # now read data for making the report
 
-render("rmarkdown_test.Rmd", params = list(
+render("rmarkdown_test.Rmd",
+    rdocx_document(reference_docx = "custom-reference.docx"),
+    params = list(
     SAN = opt$SAN,
     Sample_ID = opt$SampleID,
     Host = opt$Host,
     H_type = opt$H_type,
     Cleavage_site = opt$CleavageSite,
     phylo_tree_Src = opt$tree,
-    aa_seq_src = opt$HA_gene,
-    Sequence_Datasets = opt$datasets
+    aa_seq_src = opt$HA_gene
     ))
+
